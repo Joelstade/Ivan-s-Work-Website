4e4abe46a60cb50eed3cbc253db926e199abaecc
GrimRend <joelstade99@gmail.com>
2025-09-05 23:11:07 +0300

    Basic structure of website implemented

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar.component';
+import { Footer } from './components/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, Navbar, Footer],
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss']
+})
+
+export class App {}
